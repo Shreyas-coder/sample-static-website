73e9d283c596e0064af57c964710b8376829b2c0
Shreyas-coder <51818433+Shreyas-coder@users.noreply.github.com>
2020-12-05 17:52:29 +0530

    Add files via upload

---
+++ static.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    
+    public static void main(String[] args) throws IOException {
+       // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("F:\\EcommerceSpring\\HackerRankProblem")));
+
+       
+        
+    }
+}
